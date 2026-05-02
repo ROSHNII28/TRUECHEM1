86d46a6e875c6a5dc5094bfebaa58123235bf030
YourGitHubUsername <YourEmail@example.com>
2026-05-02 11:36:25 +0530

    update

--- truechem-main/src/pages/Products.jsx
+++ truechem-main/src/pages/Products.jsx
@@ -15,6 +15,7 @@ const constructionProducts = [
     docs: [
       { company: 'HLC RDP', type: 'TDS', file: '/RD POWDER TDS (2).pdf' },
       { company: 'JINJI RDP', type: 'TDS', file: '/MSDS RDP (JINJI).pdf' },
+      { company: 'JINJI CHEMICAL RDP', type: 'TDS', file: '/truechem-main/public/RDP TDS.pdf' },
       { company: 'YiChengJiaHua RDP', type: 'TDS', file: '/RDP HEBEI TDS (1).pdf' },
     ],
   },
@@ -26,6 +27,8 @@ const constructionProducts = [
     indiamart: 'https://www.indiamart.com/true-chem/methyl-hydroxyethyl-cellulose.html#2856799874530',
     docs: [
       { company: 'JINJI',  type: 'TDS', file: '/MHEC JINJI TDS.pdf' },
+      { company: 'JINJI CHEMICAL',  type: 'TDS', file: '/truechem-main/public/HPMC TDS.pdf' },
+       { company: 'JINJI CHEMICAL',  type: 'TDS', file: '/truechem-main/public/MHEC TDS.pdf' },
       { company: 'JINJI',  type: 'TDS', file: '/MSDS of MHEC (JINJI).pdf' },
       { company: 'Green MHEC ',   type: 'TDS', file: '/OSHA SDS MHEC-Green.docx.pdf' },
       { company: 'HPMC',   type: 'TDS', file: '/HPMC TDS (2).pdf' },
